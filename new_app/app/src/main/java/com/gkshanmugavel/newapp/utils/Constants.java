03c085f954f8cd78812802818324b051043e5074
shanm_ubuntu <gkshanmugavel.dev@gmail.com>
2018-08-07 11:26:03 +0530

    root change

--- new_app/app/src/main/java/com/gkshanmugavel/newapp/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.gkshanmugavel.newapp.utils;
-
-public class Constants {
-    //Base url
-    public final static String BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
-
-}
